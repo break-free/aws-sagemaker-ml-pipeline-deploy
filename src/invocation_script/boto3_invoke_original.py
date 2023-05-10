aa854e12424842386feacaf7bfb18ed11bbef33d
zgalford <zgalford@gmail.com>
2023-05-10 17:07:19 -0500

    saving screenshots + adding in different expected outcomes

--- src/invocation_script/boto3_invoke_original.py
+++ src/invocation_script/boto3_invoke_original.py
@@ -12,7 +12,15 @@ There's a world-famous dataset (https://archive.ics.uci.edu/ml/datasets/iris) th
 The data is provided in a CSV format as 
     sepal length in cm | sepal width in cm | petal length in cm | petal width in cm | class (label)
 """
-payload = pd.DataFrame([[1.5,0.2,4.4,2.6]]) # 1.5cm sepal length, 0.2cm sepal width, 4.4cm petal length, 2.6cm width
+# Expect Setosa:
+payload = pd.DataFrame([[4.5,3.45,1.5,1.2]]) # 4.5cm sepal length, 3.45cm sepal width, 1.5cm petal length, 1.2cm width
+
+# Expect Versicolor:
+# payload = pd.DataFrame([[6.0,3.45,4.0,1.2]]) # 6.0cm sepal length, 3.45cm sepal width, 4.0cm petal length, 1.2cm width
+
+# Expect Virginica:
+# payload = pd.DataFrame([[7.2,3.45,6.9,1.2]]) # 7.2cm sepal length, 3.45cm sepal width, 6.9cm petal length, 1.2cm width
+
 csv_file = StringIO()
 payload.to_csv(csv_file, sep=",", header=False, index=False)
 payload_as_csv = csv_file.getvalue()
